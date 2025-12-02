5abddaec388a6e538455286eaf9423a943fc0edc
Marcus441 <psminchia4153@hotmail.com>
2025-12-02 10:30:29 +1000

    refactor(api): begin api re write

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
 
           packages = with pythonPkgs; [
             fastapi
+            pkgs.poetry
             llama-cpp-python
             python
             requests
---
+++ src/api/api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+api = FastAPI()
+
+
+def main():
+    print("hello world")
+    pass
+
+
+if __name__ == "__main__":
+    main()
