c467ae05d6350b1ef11169bbce9f23b0fee29255
Marcus441 <psminchia4153@hotmail.com>
2025-12-05 11:14:31 +1000

    feat(llm_handler): added more robust handling of llms
    - added a dedicated llm_handler module that manages state of the loaded
      model
      - Added methods to control model state, such as load and unload
    - Automated logic to download off hugging face
    - Added hugging face hub to deps
    - Stored relevant constants in config.py

--- src/jetson_copilot/api.py
+++ src/jetson_copilot/api.py
@@ -1,15 +1,45 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, HTTPException, status
+import logging
+
+from .llm_handler import ensure_model_exists, load_model, model_loaded, unload_model
+from .models import ChatRequest
 import uvicorn
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # pyright: ignore[reportUnusedParameter]
+    if ensure_model_exists():
+        try:
+            load_model()
+        except Exception as e:
+            logging.critical(f"Fatal Error: Failed to initialize LLama engine: {e}")
+            raise e
+    yield
+    unload_model()
+    logging.info("FastAPI server shutdown complete.")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def check_health():
+    if not model_loaded:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="LLM model is not loaded or initialized",
+        )
+    return {"status": "active", "model_status": "loaded"}
+
+
+@app.post("/api/chat", response_model=ChatRequest)
+def get_chat():
     pass
-    return {"status": "active"}
+    return "Hello There"
 
 
 def start():
-    """Entry point for the application script."""
     uvicorn.run("jetson_copilot.api:app", host="0.0.0.0", port=11434, reload=False)
